fa6fa33e1956880f30ad28691f917c2b48024992
Gabriel Coimbra <gabao.coimbra@gmail.com>
2021-01-20 16:53:24 -0300

    Adicionar arquivos de projeto.

---
+++ CrudUsuariosSqlServer/Models/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CrudUsuariosSqlServer.Models
+{
+    public class Contexto : DbContext 
+    {
+        // construtor da classe
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
+
---
+++ CrudUsuariosSqlServer/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudUsuariosSqlServer.Models
+{
+    [Table("Usuário")]
+
+    public class Usuario
+    {     
+            
+            [Display(Name = "Id")]
+            [Column("Id")]
+
+            public int Id { get; set; }
+
+            [Display(Name = "Nome")]
+            [Column("Nome")]
+
+            public string Nome { get; set; }
+
+
+
+            [Display(Name = "Celular")]
+            [Column("Celular")]
+
+            public string Celular { get; set; }
+
+            [Display(Name = "Email")]
+            [Column("Email")]
+
+            public string Email { get; set; }
+
+        }
+    
+}
